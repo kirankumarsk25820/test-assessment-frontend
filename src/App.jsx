b1bb22650374bc693d977f7ee4d457d7d1dc8029
Kiran Kumar S K <57290570+kirankumarsk25820@users.noreply.github.com>
2025-08-15 20:09:48 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,15 @@ import { AuthProvider } from "./context/AuthContext";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+import AdminLogin from "./pages/AdminLogin";
 export default function App() {
   return (
     <AuthProvider>
       <Router>
         <div className="flex flex-col min-h-screen">
           <Navbar />
+          
+        <Route path="/admin" element={<AdminLogin />} />
           <main className="flex-1">
             <AppRouter />
           </main>
